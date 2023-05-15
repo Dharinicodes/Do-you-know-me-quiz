0aa8b6f16f0a6ce7513fcd838d9c7d875840c2cf
Dharinicodes <20554958-Dharinicodes@users.noreply.replit.com>
2023-05-15 07:07:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var readlinesync = require("readline-sync");
+
+var score = 0
+
+var userName = readlinesync.question("May I have your name? ")
+
+var someMessage = "Hey " + userName
+console.log(someMessage)
+
+
+console.log("I have a quiz for you. Let's see how well do you know me\n")
+
+
+
+function play(question, answer) {
+  var userAnswer = readlinesync.question(question)
+  if (userAnswer === answer) {
+    console.log("You are right! +1")
+    score = score + 1
+  }
+  else {
+    console.log("You are wrong! -1")
+    score = score - 1
+  }
+}
+
+play("Which cartoon do I like the most? ", "doraemon")
+
+play("What's my most fav dish? ", "noodles")
+
+play("What's my most fav color? ", "black")
+
+play("Who's my fav actor? ", "siva karthikeyan")
+
+play("Who's my fav musician? ", "yuvan")
+
+
+console.log("Final Score: ", score)
